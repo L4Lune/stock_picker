5ee24fcd7a51cabc53fa8f6d4c8e626edb1c1d37
Jack Roudebush <roudebushjack@gmail.com>
2023-04-20 23:34:10 -0400

    Add end statement

--- stock_picker.rb
+++ stock_picker.rb
@@ -6,6 +6,7 @@ def stock_picker(values)
     values[0..-2].reduce { |lowest, price| lowest.nil? || price < lowest ? price : lowest }
         values[lowest.find_index..-1].reduce(lowest) do |acc, high_price|
             acc << [lowest.find_index, high_price - lowest]
+        end
     end
 
 end
