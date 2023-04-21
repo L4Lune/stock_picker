0208b9ff1e831326825754c89ca442242276cb1e
Jack Roudebush <roudebushjack@gmail.com>
2023-04-20 23:33:42 -0400

    Throw some nested reduce logic

--- stock_picker.rb
+++ stock_picker.rb
@@ -3,7 +3,11 @@ values = [17,3,6,9,15,8,6,1,10]
 def stock_picker(values)
     buy_sell = []
     
-    values.reduce[0..-2]
+    values[0..-2].reduce { |lowest, price| lowest.nil? || price < lowest ? price : lowest }
+        values[lowest.find_index..-1].reduce(lowest) do |acc, high_price|
+            acc << [lowest.find_index, high_price - lowest]
+    end
+
 end
 
 stock_picker(prices)
